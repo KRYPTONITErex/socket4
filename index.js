bef29488cdde9f9f55024fb7e078f30c2a42b395
Lwin Phyo <lwinphyo271395@gmail.com>
2025-02-16 15:23:16 +0700

    commit for socket testWS 1.1

--- index.js
+++ index.js
@@ -26,10 +26,13 @@ const io = socket(server, {
 io.on('connection',(socket)=>{
     console.log(' a new user is connnected : @' + socket.id);
 
+    socket.on('disconnect',()=>{
+        console.log('user disconnected : @' + socket.id);
+    })
+
     socket.on('chat',(data)=>{
         // console.log(data);
         io.sockets.emit('chat',data);
-
     })
 
     socket.on('typing',(name)=>{
